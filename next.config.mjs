92c8bd34afe3317a3e825d2dcc2b5faf5edfcadb
PreciousAdekunle784 <preciousadekunle784@gmail.com>
2026-05-04 10:45:47 -0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     reactStrictMode: true,
     poweredByHeader: false,
+    images: {
+        unoptimized: true,
+    },
     compiler: {
         removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
     },
